b0b961b53dbf0ad9c18aa227202855cdf619dab0
Qwerty8668 <hubert.zientara@gmail.com>
2025-10-20 22:37:07 +0200

    Language changed to english

--- photo_script.py
+++ photo_script.py
@@ -7,15 +7,16 @@ import os
 SERVER_URL = "ws://server_address:8080"
 
 def on_message(ws, message):
-    print("Odebrano od serwera:", message)
+    print("Received:", message)
     if message == "SEND_PHOTO":
         #os.system("libcamera-jpeg -o photo.jpg")
         with open("photo.jpg", "rb") as f:
             data = base64.b64encode(f.read()).decode("utf-8")
             ws.send(data)
-            print("Wysłano nowe zdjęcie")
+            print("New photo sent.")
     elif message.startswith("Coordinates: "):
         print(message)
+	ws.send("Coordinates received.")
     else:
         ws.send("Message send in invalid format. Accepted messages: 'SEND_PHOTO', 'Coordinates: (lat, lon)'")
 
