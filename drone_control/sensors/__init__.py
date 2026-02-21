56ec61c61904ab3ca2e5a077d46e6c8769c62fb8
JanMatusiak <jan.matusiak.2006@gmail.com>
2026-02-21 12:28:29 +0100

    create a DroneController class and remove LidarSensor class for now

--- drone_control/sensors/__init__.py
+++ drone_control/sensors/__init__.py
@@ -1,6 +1,5 @@
-from drone_control.sensors.lidar_sensor import LidarSensor
 from drone_control.sensors.photo_sensor import PhotoSensor
 from drone_control.sensors.recording_sensor import RecordingSensor
 from drone_control.sensors.telemetry_sensor import TelemetrySensor
 
-__all__ = ["PhotoSensor", "TelemetrySensor", "RecordingSensor", "LidarSensor"]
+__all__ = ["PhotoSensor", "TelemetrySensor", "RecordingSensor"]
